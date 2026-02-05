734ff97e1bdae69af4bd7fd4503bdcaa8fe3ad81
silaryblia <volodiamarkiv@gmail.com>
2026-02-05 20:51:58 +0300

    gateway 1/2

---
+++ internal/currency/gateway/currency.go
@@ -0,0 +1,70 @@
+package gateway
+
+import (
+	"Currency-apiNew2/internal/currency/domain"
+	"Currency-apiNew2/internal/currency/service"
+	"context"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type currencyGateway struct {
+	service *service.CurrencyService
+	logger  *zap.Logger
+}
+
+func NewCurrencyGateway(
+	svc *service.CurrencyService,
+	logger *zap.Logger,
+) CurrencyGateway {
+	return &currencyGateway{service: svc, logger: logger}
+}
+
+func (g *currencyGateway) GetLatest(
+	ctx context.Context,
+	code string) (domain.Currency, error) {
+
+	c, err := domain.ParseCurrencyCode(code)
+	if err != nil {
+		return domain.Currency{}, err
+	}
+	return g.service.GetLatest(ctx, c)
+}
+
+func (g *currencyGateway) GetAtDate(
+	ctx context.Context,
+	code string,
+	date time.Time) (domain.Currency, error) {
+
+	c, err := domain.ParseCurrencyCode(code)
+	if err != nil {
+		return domain.Currency{}, err
+	}
+	return g.service.GetAtDate(ctx, c, date)
+}
+
+func (g *currencyGateway) GetRange(
+	ctx context.Context,
+	code string,
+	from, to time.Time) ([]domain.Currency, error) {
+
+	if from.After(to) {
+		return nil, domain.ErrInvalidArg
+	}
+	c, err := domain.ParseCurrencyCode(code)
+	if err != nil {
+		return nil, err
+	}
+
+	return g.service.GetRange(ctx, c, from, to)
+}
+
+func (g *currencyGateway) GetAll(
+	ctx context.Context) (map[domain.CurrencyCode]domain.Currency, error) {
+	return g.service.GetAll(ctx)
+}
+
+func (g *currencyGateway) SyncRates(ctx context.Context) error {
+	return g.service.SyncRates(ctx)
+}
---
+++ internal/currency/gateway/interface.go
@@ -0,0 +1,15 @@
+package gateway
+
+import (
+	"Currency-apiNew2/internal/currency/domain"
+	"context"
+	"time"
+)
+
+type CurrencyGateway interface {
+	GetLatest(ctx context.Context, code string) (domain.Currency, error)
+	GetAtDate(ctx context.Context, code string, date time.Time) (domain.Currency, error)
+	GetRange(ctx context.Context, code string, from, to time.Time) ([]domain.Currency, error)
+	GetAll(ctx context.Context) (map[domain.CurrencyCode]domain.Currency, error)
+	SyncRates(ctx context.Context) error
+}
